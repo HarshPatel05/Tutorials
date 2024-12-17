83ccbfda0c4c751ae7600511d51527aad0d130c7
Harsh Patel <04harshpatel@gmail.com>
2024-12-17 14:30:34 -0600

    Initial commit

---
+++ JavaScript/index.js
@@ -0,0 +1,13 @@
+console.log("Hello, World!")
+
+/** Data Types:
+ * undefined, null, boolean, string, symbol, number, and object
+*/
+
+let Ourname = 8;
+
+console.log(Ourname)
+
+var myname = "My name is Harsh."
+
+console.log(myname)
---
+++ Node/app.js
@@ -0,0 +1,11 @@
+function sayHello(name) {
+    console.log('Hello ' + name)
+}
+
+// sayHello('Harsh');
+
+// console.log(module);
+
+var log = require('./logger');
+
+log('message');

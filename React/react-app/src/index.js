cb30016c76ca6bd2f5251d2bb7edc92cf84a9f78
Harsh Patel <04harshpatel@gmail.com>
2024-12-17 20:44:24 -0600

    starting out...

--- React/react-app/src/index.js
+++ React/react-app/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 
 const element = <h1>Hello World</h1>;
-console.log(element);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);
